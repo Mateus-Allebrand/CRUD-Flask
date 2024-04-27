220f758330c6af1ae816a37af614c470200e8637
Mateus <125222236+Mateus-Allebrand@users.noreply.github.com>
2024-04-27 20:52:26 -0300

    update 20 curso 02 flask

--- jogoteca.py
+++ jogoteca.py
@@ -1,40 +1,42 @@
 from flask import Flask,render_template, request, redirect,session,flash,url_for
+from flask_sqlalchemy import SQLAlchemy
 
 
-class Jogo:
-    def __init__(self, nome, categoria, console):
+#no inicio usei esse codigo para criar as classes, agora foi substituido por classe jogos e usuarios com db
+# class Jogo:
+#     def __init__(self, nome, categoria, console):
 
-        self.nome = nome
-        self.categoria = categoria
-        self.console = console
+#         self.nome = nome
+#         self.categoria = categoria
+#         self.console = console
 
-    # def __str__(self):                  #Dessa maneira daria para resolver
-    #     return f"Nome: {self.nome}"
+#     # def __str__(self):                  #Dessa maneira daria para resolver
+#     #     return f"Nome: {self.nome}"
     
-jogo01 = Jogo("Tetris", "puzly", "Atary")
-jogo02 = Jogo("God of war", "Rack n slash", "PS2")
-jogo03 = Jogo("Mortal kombat", "Luta", "PS2")
+# jogo01 = Jogo("Tetris", "puzly", "Atary")
+# jogo02 = Jogo("God of war", "Rack n slash", "PS2")
+# jogo03 = Jogo("Mortal kombat", "Luta", "PS2")
 
 
-lista_jogos = [jogo01,jogo02,jogo03]
+# lista_jogos = [jogo01,jogo02,jogo03]
 
 
-class Usuario:
-    def __init__(self, nome, nickname, senha):
+# class Usuario:
+#     def __init__(self, nome, nickname, senha):
         
-        self.nome = nome
-        self.nickname = nickname
-        self.senha = senha
+#         self.nome = nome
+#         self.nickname = nickname
+#         self.senha = senha
 
     
 
-usuario01 = Usuario("Mateus Allebrand", "Mat", "1234")
-usuario02 = Usuario("Mariana Alves", "Mari", "4567")
-usuario03 = Usuario("Gabrielli Sena", "Gabi", "1236")
+# usuario01 = Usuario("Mateus Allebrand", "Mat", "1234")
+# usuario02 = Usuario("Mariana Alves", "Mari", "4567")
+# usuario03 = Usuario("Gabrielli Sena", "Gabi", "1236")
 
-usuarios ={usuario01.nome:usuario01,
-           usuario02.nome:usuario02,
-           usuario03.nome:usuario03}  
+# usuarios ={usuario01.nome:usuario01,
+#            usuario02.nome:usuario02,
+#            usuario03.nome:usuario03}  
  
 
 
@@ -45,11 +47,45 @@ usuarios ={usuario01.nome:usuario01,
 app = Flask(__name__)
 app.secret_key = "alumais"
 
+
+app.config["SQLALCHEMY_DATABASE_URI"] = \
+"{SGBD}://{usuario}:{senha}@{servidor}/{database}".format(
+SGBD = "mysql+mysqlconnector",
+usuario = "root",
+senha = "96465840Mt?",
+servidor = "localhost",
+database = "jogoteca"
+)
+
+db = SQLAlchemy(app)
+
+class Jogos(db.Model):
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    nome = db.Column(db.String(50),nullable=False)
+    categoria = db.Column(db.String(40), nullable=False)
+    console = db.Column(db.String(20),nullable=False)
+
+    def __repr__(self):
+        return "<name %r" %self.name
+
+
+class Usuarios(db.Model):
+    nickname = db.Column(db.String(20),primary_key=True)
+    nome = db.Column(db.String(50),nullable=False)
+    senha = db.Column(db.String(100),nullable=False)
+    
+
+    def __repr__(self):
+        return "<name %r" %self.name
+
+
+
+
 #aqui estou criando uma rota, tenho que criar uma função para dizer o que vai ter nessa rota
 @app.route("/")
 def index():
-    
-    return render_template("lista.html",titulo="Jogos", jogos = lista_jogos)  # como não estou lidando apenas com um terminal, preciso colocar aas tags, pois na web lingaugem como "hello World" sem tag, não seria intendido
+    lista = Jogos.query.order_by(Jogos.id)
+    return render_template("lista.html",titulo="Jogos", jogos = lista)  # como não estou lidando apenas com um terminal, preciso colocar aas tags, pois na web lingaugem como "hello World" sem tag, não seria intendido
 
 
 @app.route("/novo")
@@ -63,11 +99,18 @@ def criar():
     nome = request.form["nome"]
     categoria = request.form["categoria"]
     console = request.form["console"]
-    jogo = Jogo(nome,categoria,console)
-    lista_jogos.append(jogo)
+     
+    #verificando se o jogo que vou incluir já existe em meu banco de dados    #cod antes era assim =>  jogo = Jogo(nome,categoria,console)
+    jogo = Jogos.query.filter_by(nome=nome).first()                           #cod antes era assim =>  lista_jogos.append(jogo)
 
-    # return render_template("lista.html", titulo = "Jogos", jogos = lista_jogos) #ao invés desse codigo vou usar redirect
-    return redirect(url_for("index"))
+    if jogo:
+        flash("O jogo já existe na lista")
+        return redirect(url_for("index"))                                                          
+                                                                            # return render_template("lista.html", titulo = "Jogos", jogos = lista_jogos) #ao invés desse codigo vou usar redirect
+    novo_jogo = Jogos(nome=nome, categoria=categoria,console=console)
+    db.session.add(novo_jogo)
+    db.session.commit()
+    return redirect(url_for("index")) 
 
 @app.route("/login")
 def login():
@@ -77,9 +120,10 @@ def login():
 
 @app.route("/autenticar", methods=["POST",])
 def autenticar():
-    if request.form["usuario"] in usuarios:
-        usuario = usuarios[request.form["usuario"]]
-        
+    usuario = Usuarios.query.filter_by(nickname=request.form["usuario"]).first()
+
+    if usuario:                          #nessa linha eras assim => request.form["usuario"] in usuarios: antes do db
+                                         #nessa linha eras assim => usuario = usuarios[request.form["usuario"]] antes do db
         if request.form["senha"] == usuario.senha:
             session["Usuario_logado"] = usuario.nickname 
             flash(usuario.nickname + "Logado com sucesso!")
--- prapara_banco.py
+++ prapara_banco.py
@@ -2,93 +2,95 @@ import mysql.connector
 from mysql.connector import errorcode
 
 print("Conectando...")
-
 try:
-    conn = mysql.connector.connect(
-        host = "localhost",
-        user ="root",
-        password = "96465840Mt?"
-    )
-    print("Conexão bem sucedida!")
-
+      conn = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='96465840Mt?'
+      )
 except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Erro! Há algo errado com o nome do usuário ou senha")
-
-    else:
-        print(err)
+      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print('Existe algo errado no nome de usuário ou senha')
+      else:
+            print(err)
 
 cursor = conn.cursor()
 
-# Criar o banco de dados 'jogoteca' se ele não existir
-cursor.execute("CREATE DATABASE IF NOT EXISTS jogoteca;")
-print("Banco de dados 'jogoteca' criado com sucesso!")
+cursor.execute("DROP DATABASE IF EXISTS `jogoteca`;")
+
+cursor.execute("CREATE DATABASE `jogoteca`;")
+
+cursor.execute("USE `jogoteca`;")
 
 
-cursor.execute("USE jogoteca;")
-
-#criando tabelas
-
+# criando tabelas
 TABLES = {}
+TABLES['Jogos'] = ('''
+      CREATE TABLE `jogos` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `nome` varchar(50) NOT NULL,
+      `categoria` varchar(40) NOT NULL,
+      `console` varchar(20) NOT NULL,
+      PRIMARY KEY (`id`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
 
-TABLES["Jogos"] = ("""
-    CREATE TABLE jogos (
-        'id' int(11) = NOT NULL AUTO_INCREMENT, 
-        'nome' varchar(50) NOT NULL,
-        'categoria' varchar(40) NOT NULL,
-        'console' varchar(20) NOT NULL,
-        PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;""")
-
-TABLES["Usuarios"] =("""
-    CREATE TABLE Usuarios(
-    'nome' varchar(30) NOT NULL,        
-    'nickname' varchar(20) NOT NULL,
-    'senha' varchar(100) NOT NULL,
-    PRIMARY KEY (nickname)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;""")
- 
+TABLES['Usuarios'] = ('''
+      CREATE TABLE `usuarios` (
+      `nome` varchar(20) NOT NULL,
+      `nickname` varchar(8) NOT NULL,
+      `senha` varchar(100) NOT NULL,
+      PRIMARY KEY (`nickname`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
 
 for tabela_nome in TABLES:
-    tabela_sql = TABLES[tabela_nome]
-    try:
-        print(f"Criando tabela {tabela_sql}",end=" ")
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-            print("Tabela já existe! ")
-        else:
-            print(err.msg)
-    else:
-        print("OK")
+      tabela_sql = TABLES[tabela_nome]
+      try:
+            print('Criando tabela {}:'.format(tabela_nome), end=' ')
+            cursor.execute(tabela_sql)
+      except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                  print('Já existe')
+            else:
+                  print(err.msg)
+      else:
+            print('OK')
 
-#inserindo usuários
-usuario_sql = "INSERT INTO usuarios (nome, nickname, senha) VALUES (%s,%s,%s);"
-usuarios = [("Mateus Allebrand", "Mat", "1234"),
-("Mariana Alves", "Mari", "4567"),
-("Gabrielli Sena", "Gabi", "1236"),
+# inserindo usuarios
+usuario_sql = 'INSERT INTO usuarios (nome, nickname, senha) VALUES (%s, %s, %s)'
+usuarios = [
+    ("Bruno Divino", "BD", "alohomora"),
+    ("Camila Ferreira", "Mila", "paozinho"),
+    ("Guilherme Louro", "Cake", "python_eh_vida"),
+    ("Mateus Allebrand", "Mat", "1234"),
+    ("Mariana Alves", "Mari", "4567"),
+    ("Gabrielli Sena", "Gabi", "1236")
 ]
+cursor.executemany(usuario_sql, usuarios)
 
-#executemany() está sendo chamado em um objeto do tipo cursor para executar uma instrução SQL usuario_sql várias vezes, uma para cada conjunto de parâmetros na lista usuarios
-cursor.executemany(usuario_sql,usuarios)
-
-cursor.execute("SELECT * FROM jogoteca.usuarios;")
-print("-=-=-=-=- USUARIOS -=-=-=-=-")
-
-for user in cursor.fetchall:
+cursor.execute('select * from jogoteca.usuarios')
+print(' -------------  Usuários:  -------------')
+for user in cursor.fetchall():
     print(user[1])
 
-
-#inserindo jogos
-jogos_sql = "INSERT INTO jogos (nome, categoria, console) VALUES (%s,%s,%s);"
-jogos =[
-("Tetris", "puzly", "Atary"),
-("God of war", "Rack n slash", "PS2"),
-("Mortal kombat", "Luta", "PS2")
+# inserindo jogos
+jogos_sql = 'INSERT INTO jogos (nome, categoria, console) VALUES (%s, %s, %s)'
+jogos = [
+      ('Tetris', 'Puzzle', 'Atari'),
+      ('God of War', 'Hack n Slash', 'PS2'),
+      ('Mortal Kombat', 'Luta', 'PS2'),
+      ('Valorant', 'FPS', 'PC'),
+      ('Crash Bandicoot', 'Hack n Slash', 'PS2'),
+      ('Need for Speed', 'Corrida', 'PS2'),
 ]
+cursor.executemany(jogos_sql, jogos)
 
-cursor.executemany(jogos_sql,jogos)
-
-for jogo in cursor.fetchall:
+cursor.execute('select * from jogoteca.jogos')
+print(' -------------  Jogos:  -------------')
+for jogo in cursor.fetchall():
     print(jogo[1])
 
+# commitando se não nada tem efeito
+conn.commit()
+
+cursor.close()
 conn.close()
