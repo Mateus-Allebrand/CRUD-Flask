04110fb560d1ff0ffb4440301d5b9302b1d26a99
Mateus <125222236+Mateus-Allebrand@users.noreply.github.com>
2024-05-01 23:44:15 -0300

    Update 01/05/2024

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from jogoteca import app
+import os
 
 
 SECRET_KEY = "alumais"  #era assim essa parte do codigo: => app.secret_key = "alumais"
@@ -6,3 +7,5 @@ SECRET_KEY = "alumais"  #era assim essa parte do codigo: => app.secret_key = "al
                 #era assim essa parte do codigo: =>         app.config"SQLALCHEMY_DATABASE_URI"
 
 SQLALCHEMY_DATABASE_URI = "{SGBD}://{usuario}:{senha}@{servidor}/{database}".format(SGBD = "mysql+mysqlconnector",usuario = "root",senha = "96465840Mt?", servidor = "localhost",database = "jogoteca")
+
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + "/uploads"
--- views.py
+++ views.py
@@ -1,6 +1,9 @@
-from flask import render_template, request, redirect,session,flash,url_for
+from flask import render_template, request, redirect,session,flash,url_for, send_from_directory
 from jogoteca import app,db
 from models import Jogos, Usuarios
+from helpers import recupera_imagem,deleta_arquivo
+import time
+
 
 #aqui estou criando uma rota, tenho que criar uma função para dizer o que vai ter nessa rota
 @app.route("/")
@@ -8,7 +11,7 @@ def index():
     lista = Jogos.query.order_by(Jogos.id)
     return render_template("lista.html",titulo="Jogos", jogos = lista)  # como não estou lidando apenas com um terminal, preciso colocar aas tags, pois na web lingaugem como "hello World" sem tag, não seria intendido
 
-
+#
 @app.route("/novo")
 def novo():
     if "Usuario_logado" not in session or session["Usuario_logado"] == None: 
@@ -31,6 +34,13 @@ def criar():
     novo_jogo = Jogos(nome=nome, categoria=categoria,console=console)
     db.session.add(novo_jogo)
     db.session.commit()
+
+    arquivo = request.files["arquivo"]
+    #arquivo.save(f"uploads/{arquivo.filename}") vou melhorar isso para não dar conflito
+    upload_path = app.config["UPLOAD_PATH"]
+    timestemp = time.time()
+    arquivo.save(f"{upload_path}/capa{novo_jogo.id}-{timestemp}.jpg")
+
     return redirect(url_for("index")) 
 
 @app.route("/editar/<int:id>")
@@ -38,7 +48,18 @@ def editar(id):
     if "Usuario_logado" not in session or session["Usuario_logado"] == None: 
         return redirect(url_for("login", proxima = url_for("editar"))) #return redirect("/login?proxima=novo") 
     jogo = Jogos.query.filter_by(id=id).first()
-    return render_template("editar.html", titulo = "editando Jogo", jogo=jogo)
+
+    capa_jogo = recupera_imagem(id)
+
+    return render_template("editar.html", titulo = "editando Jogo", jogo=jogo, capa_jogo=capa_jogo)
+
+# @app.route('/editar/<int:id>')
+# def editar(id):
+#     if 'usuario_logado' not in session or session['usuario_logado'] == None:
+#         return redirect(url_for('login', proxima=url_for('editar', id=id)))
+#     jogo = Jogos.query.filter_by(id=id).first()
+#     capa_jogo = recupera_imagem(id)
+#     return render_template('editar.html', titulo='Editando Jogo', jogo=jogo, capa_jogo=capa_jogo)
 
 @app.route("/atualizar", methods = ["POST",])
 def atualizar():
@@ -49,6 +70,12 @@ def atualizar():
 
     db.session.add(jogo)
     db.session.commit()
+
+    arquivo = request.files["arquivo"]
+    upload_path = app.config["UPLOAD_PATH"]
+    timestamp = time.time()
+    deleta_arquivo(jogo.id)
+    arquivo.save(f"{upload_path}/capa{jogo.id}-{timestamp}.jpg")
 
     return redirect(url_for("index"))
 
@@ -92,3 +119,7 @@ def logout():
     session["Usuario_logado"] = None
     flash("Logout efefuado com sucesso! ")
     return redirect(url_for("index"))
+
+@app.route("/uploads/<nome_arquivo>")
+def imagem(nome_arquivo):
+    return send_from_directory("uploads", nome_arquivo)
